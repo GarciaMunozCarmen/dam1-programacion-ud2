0360dcadddca17a43b521659a8bc9bcb800bf066
GarciaMunozCarmen <garciamunozcarmen18@gmail.com>
2025-10-07 14:06:47 +0200

    Create TrabajandoConMetodos.java

---
+++ src/TrabajandoConMetodos.java
@@ -0,0 +1,70 @@
+import utilidades.Cancion;
+import utilidades.DemoMetodos;
+
+public class TrabajandoConMetodos {
+    public static void main(String[] args) {
+        DemoMetodos method = new DemoMetodos();
+        
+        // ================================
+        // EJERCICIO 1: Métodos void
+        // ================================
+        // 1. Llama al método saludar() de la clase DemoMetodos.
+        // Tu código aquí ↓
+
+         method.saludar();
+
+        // ================================
+        // EJERCICIO 2: Métodos con retorno
+        // ================================
+        // 1. Llama al método sumar(5, 3).
+        // 2. Guarda el resultado en una variable e imprímelo.
+        // 3. Llama al método presentar("Lucía") e imprime el resultado.
+        // Tu código aquí ↓
+         
+         int suma = method.sumar(5, 3);
+         System.out.println(suma);
+         System.out.println(method.presentar("Lucía"));
+
+        // ================================
+        // EJERCICIO 3: Paso por valor (primitivos)
+        // ================================
+        // 1. Declara una variable int n = 10.
+        // 2. Llama a method.incrementar(n).
+        // 3. Imprime n después de llamar al método.
+        // Tu código aquí ↓
+         
+         int n = 10;
+         method.incrementar(n);
+         System.out.println(n);
+
+        // ================================
+        // EJERCICIO 4: Paso por referencia (objetos)
+        // ================================
+        // 1. Crea un objeto Cancion con título "Original" - Usa el constructor parametrizado
+        //  que recibe 2 argumentos: título y artista.
+        // 2. Llama a method.cambiarTitulo(cancion).
+        // 3. Imprime la canción después.
+        // Tu código aquí ↓
+         Cancion c1 = new Cancion("Original", "Yo");
+         method.cambiarTitulo(c1);
+         System.out.println(c1);
+    }
+}
+
+// ================================
+// RESPUESTAS DE REFLEXIÓN
+// ================================
+// Ejercicio 1: ¿Qué ocurre al llamar a un método void como saludar()?
+// Respuesta: Que no te devuelve nada
+
+// Ejercicio 2: ¿Qué diferencia hay entre un método void y uno con retorno?
+// Respuesta: Un método void no devuelve nada y uno con retorno te devuelve algo, por ejemplo el método suma te devuleve la suma de dos números
+
+// Ejercicio 3: ¿Ha cambiado la variable n fuera del método incrementar()?
+// ¿Por qué?
+// Respuesta: NO LO SE
+
+// Ejercicio 4: ¿Qué ha ocurrido con el título de la Cancion al usar cambiarTitulo()?
+// ¿Por qué es diferente al caso del int?
+// Respuesta: Se ha cambiado el titulo de la canción 
+
